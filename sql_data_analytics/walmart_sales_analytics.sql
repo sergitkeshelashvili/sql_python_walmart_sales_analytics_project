db085acbd2fda14632c7dab5fcae84ff3d201bb8
@Sergi <sergitkeshelashvili@gmail.com>
2025-07-01 22:58:00 +0400

    Update walmart_sales_analytics.sql

--- sql_data_analytics/walmart_sales_analytics.sql
+++ sql_data_analytics/walmart_sales_analytics.sql
@@ -21,6 +21,8 @@ Time-Based Analysis
 -- Retrieves all tables in the database from the INFORMATION_SCHEMA
 SELECT * FROM INFORMATION_SCHEMA.TABLES;
 
+===========================================================
+
 -- Retrieves all columns in the database, providing details like table name, column name, and data type
 SELECT * FROM INFORMATION_SCHEMA.COLUMNS;
 
@@ -34,14 +36,20 @@ SELECT * FROM INFORMATION_SCHEMA.COLUMNS;
 SELECT DISTINCT branch
 FROM walmart_sales;
 
+===========================================================
+    
 -- Retrieves unique cities in the walmart_sales table
 SELECT DISTINCT city
 FROM walmart_sales;
 
+===========================================================
+    
 -- Retrieves unique product categories in the walmart_sales table
 SELECT DISTINCT category
 FROM walmart_sales;
 
+===========================================================
+    
 -- Retrieves unique payment methods used in the walmart_sales table
 SELECT DISTINCT payment_method
 FROM walmart_sales;
@@ -59,30 +67,42 @@ SELECT
     EXTRACT(YEAR FROM AGE(MAX(datetime), MIN(datetime))) AS sales_range_years
 FROM walmart_sales;
 
+===========================================================
+    
 -- Computes the total sales revenue by multiplying unit price and quantity
 SELECT 
     SUM(unit_price * quantity) AS total_sales
 FROM walmart_sales;
+
+===========================================================
 
 -- Calculates the total number of items sold
 SELECT 
     SUM(quantity) AS total_items_sold
 FROM walmart_sales;
 
+===========================================================
+
 -- Computes the average unit price of items sold
 SELECT
     AVG(unit_price) AS avg_price
 FROM walmart_sales;
+
+===========================================================
 
 -- Calculates the total profit by factoring in the profit margin
 SELECT
     SUM(unit_price * quantity * profit_margin) AS total_profit
 FROM walmart_sales;
 
+===========================================================
+
 -- Computes the average customer rating for all transactions
 SELECT 
     AVG(rating) AS avg_rating
 FROM walmart_sales;
+
+===========================================================
 
 -- Generates a consolidated report of key metrics: total sales, total quantity, average price, total profit, and average rating
 SELECT 'Total Sales' AS measure_name, SUM((unit_price * quantity)) AS measure_value FROM walmart_sales
@@ -119,6 +139,8 @@ SELECT
 FROM place_data
 LIMIT 10;
 
+===========================================================
+
 -- Ranks product categories by total sales
 WITH category_data AS (
     SELECT
@@ -134,6 +156,8 @@ SELECT
     RANK() OVER(ORDER BY total_sales DESC) AS total_sales_rank_by_category
 FROM category_data;
 
+===========================================================
+
 -- Calculates total sales by payment method
 SELECT
     payment_method,
@@ -141,6 +165,8 @@ SELECT
 FROM walmart_sales
 GROUP BY payment_method
 ORDER BY total_sales DESC;
+
+===========================================================
 
 -- Ranks payment methods by total sales and number of transactions
 WITH payment_data AS (
@@ -160,6 +186,8 @@ SELECT
     RANK() OVER(ORDER BY total_transactions DESC) AS total_tr_rank_by_payment_method
 FROM payment_data;
 
+===========================================================
+
 -- Identifies the highest-rated product category for each branch
 SELECT
     *
@@ -175,6 +203,8 @@ FROM (
 )
 WHERE rank = 1;
 
+===========================================================
+
 -- Determines the busiest day for each branch based on the number of transactions
 SELECT
     *
@@ -189,6 +219,8 @@ FROM (
 )
 WHERE RANK = 1;
 
+===========================================================
+
 -- Analyzes transaction counts by day of the week across all branches
 SELECT
     TO_CHAR(datetime, 'Day') AS day_name,
@@ -196,6 +228,8 @@ SELECT
 FROM walmart_sales
 GROUP BY day_name
 ORDER BY number_of_transactions DESC;
+
+===========================================================
 
 -- Ranks cities and categories by average rating, showing min, max, and average ratings for the top 10
 WITH rating_data AS (
@@ -218,6 +252,8 @@ SELECT
 FROM rating_data
 LIMIT 10;
 
+===========================================================
+
 -- Ranks product categories by total profit
 WITH category_profit AS (
     SELECT
@@ -232,6 +268,8 @@ SELECT
     total_profit,
     RANK() OVER(ORDER BY total_profit DESC) AS total_profit_rank
 FROM category_profit;
+
+===========================================================
 
 -- Ranks payment methods by transaction count within each branch
 SELECT
@@ -259,6 +297,8 @@ FROM walmart_sales
 GROUP BY EXTRACT(YEAR FROM datetime), TO_CHAR(datetime, 'MON')
 ORDER BY EXTRACT(YEAR FROM datetime), TO_CHAR(datetime, 'MON');
 
+===========================================================
+
 -- Summarizes sales, transactions, and quantity sold by year and month (using month number)
 SELECT
     EXTRACT(YEAR FROM datetime) AS transaction_year,
@@ -269,6 +309,8 @@ SELECT
 FROM walmart_sales
 GROUP BY EXTRACT(YEAR FROM datetime), EXTRACT(MONTH FROM datetime)
 ORDER BY EXTRACT(YEAR FROM datetime), EXTRACT(MONTH FROM datetime);
+
+===========================================================
 
 -- Calculates total sales per month and computes a running total and moving average price over time
 SELECT
